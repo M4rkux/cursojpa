a5539ab37833b42c33340668688927170d7a03b6
Marcus <M4rkux@gmail.com>
2014-08-30 17:38:41 -0400

    Aula 3 - Cap, 5 pag. 27

--- códigofonte/src/br/curso/jpa/bean/Conta.java
+++ códigofonte/src/br/curso/jpa/bean/Conta.java
@@ -1,8 +1,12 @@
 package br.curso.jpa.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Conta {
@@ -11,6 +15,15 @@ public class Conta {
 	private Long Id;
 	private String titular;
 	private Double saldo;
+	@OneToMany(mappedBy="conta")
+	private List<Movimentacao> listaDeMovimentacoes = new ArrayList<Movimentacao>();
+	
+	public List<Movimentacao> getListaDeMovimentacoes() {
+		return listaDeMovimentacoes;
+	}
+	public void setListaDeMovimentacoes(List<Movimentacao> listaDeMovimentacoes) {
+		this.listaDeMovimentacoes = listaDeMovimentacoes;
+	}
 	public Long getId() {
 		return Id;
 	}
